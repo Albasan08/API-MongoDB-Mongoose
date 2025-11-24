3735a91a30529dd1f82d52582166d0392f1d09bc
Albasan08 <sandin.alba@gmail.com>
2025-11-24 18:30:56 +0100

    ADD: getAServicebyID, getAllServices

--- src/controllers/servicios.controller.js
+++ src/controllers/servicios.controller.js
@@ -32,21 +32,70 @@ const createAService = async (req, res) => {
 };
 
 // GET ALL SERVICES
-const getAllServices = (req, res) => {
+const getAllServices = async (req, res) => {
     
     try {
+        const serviciosBuscados = await Servicio.find();
+        console.log(serviciosBuscados);
+
+        if(serviciosBuscados) {
+            return res.status(200).json({
+            ok: true,
+            mensaje: "Servicios encontrados correctamente",
+            data: serviciosBuscados
+        })
+        } else {
+            return res.status(404).json(
+                {
+                    ok: false,
+                    message: "Servicios no encontrados"
+            })
+        }
 
     } catch (error) {
+        // Siempre poner un console.log para el administrador poder identificar el error
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            mensaje: "Contacte con el administrador"
+        });
 
     }
 };
 
 // GET A SERVICE BY ID
-const getAServiceById = (req, res) => {
+const getAServiceById = async (req, res) => {
     
+    const {id} = req.params;
+    //console.log(id);
+
     try {
 
+        const servicioBuscado = await Servicio.findById(id);
+        console.log(servicioBuscado);
+
+        if(servicioBuscado) {
+            return res.status(200).json({
+            ok: true,
+            mensaje: "Servicio encontrado correctamente por su ID",
+            data: servicioBuscado
+        })
+        } else {
+            return res.status(404).json(
+                {
+                    ok: false,
+                    message: "Servicio no encontrado"
+            })
+        }
+
+
     } catch(error) {
+        // Siempre poner un console.log para el administrador poder identificar el error
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            mensaje: "Contacte con el administrador"
+        });
 
     }
 };
